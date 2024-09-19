6f5076623ef34aa5b7370202d54cfc11ea50fa98
Aditya Kumar Mishra <adityakr159@gmail.com>
2024-09-19 23:25:04 +0530

    level design

--- Mobs/soldier_a.gd
+++ Mobs/soldier_a.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed = 800
+@export var speed = 80
 # Called when the node enters the scene tree for the first time.
 #func _ready():
 	#pass # Replace with function body.
