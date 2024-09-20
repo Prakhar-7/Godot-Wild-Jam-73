1bc4e0ae66de1542a76cf5dcb9a714d9d7c4de4c
Prakhar Srivastava <sriprakhar777@gmail.com>
2024-09-20 13:08:29 +0530

    Add Castle & Arrow

---
+++ Tower/castle.gd
@@ -0,0 +1,41 @@
+extends StaticBody2D
+
+var Arrow = preload("res://Tower/Arrow.tscn")
+var ArrowDamage = 5
+var pathName 
+var currTargets = []
+var curr
+
+
+
+
+func _on_tower_body_entered(body):
+	if "soldier_a" in body.name: 
+		var tempArray = []
+		currTargets = get_node("Tower")
+		
+		for i in currTargets:
+			if "soldier" in i.name:
+				tempArray.append(i)
+				
+		var currTarget = null
+		
+		for i in tempArray:
+			if currTarget == null:
+				currTarget = i.get_node("../")
+			else:
+				if i.get_parent().get_progress() > currTarget.get_progress():
+					currTarget = i.get_node("../")
+		
+		curr = currTarget
+		pathName = currTarget.get_parrent().name
+		
+		var tempArrow = Arrow.instantiate()
+		tempArrow.pathName = pathName
+		tempArrow.ArrowDamage = ArrowDamage
+		get_node("ArrowContainer").add_child(tempArrow)
+		tempArrow.global_position = $Aim.global_position
+
+
+func _on_tower_body_exited(body):
+	pass # Replace with function body.
