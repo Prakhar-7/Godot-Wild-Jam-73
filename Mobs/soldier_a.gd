03caf4b0bddb6ed4afb5077191250e2a5975635e
Aditya Kumar Mishra <adityakr159@gmail.com>
2024-09-20 16:40:01 +0530

    code

--- Mobs/soldier_a.gd
+++ Mobs/soldier_a.gd
@@ -5,7 +5,6 @@ extends CharacterBody2D
 #func _ready():
 	#pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	get_parent().set_progress(get_parent().get_progress() + speed*delta)
--- scripts/castle.gd
+++ scripts/castle.gd
@@ -6,34 +6,45 @@ var pathName
 var currTargets = []
 var curr
 
-
 func _on_tower_body_entered(body):
-	if "soldier A" in body.name: 
+	if "Soldier A" in body.name: 
+		print("body entered")
 		var tempArray = []
-		currTargets = get_node("Tower")
+		currTargets = get_node("Tower").get_overlapping_bodies()  # Get overlapping bodies
 		
+		# Collect all targets whose name contains "soldier"
 		for i in currTargets:
-			if "soldier" in i.name:
+			if "Soldier" in i.name:
 				tempArray.append(i)
-				
+				print(i.name)
+		
 		var currTarget = null
 		
+		# Choose the target based on progress
 		for i in tempArray:
-			if currTarget == null:
-				currTarget = i.get_node("../")
-			else:
-				if i.get_parent().get_progress() > currTarget.get_progress():
-					currTarget = i.get_node("../")
+			print(i.get_parent())
+			var parent_node = i.get_parent()  # Get the parent node
+			if parent_node and parent_node.has_method("get_progress"):  # Ensure method exists
+				if currTarget == null:
+					currTarget = parent_node
+				else:
+					if parent_node.get_progress() > currTarget.get_progress():
+						currTarget = parent_node
 		
-		curr = currTarget
-		pathName = currTarget.get_parrent().name
+		if currTarget != null:
+			pathName = currTarget.name
+			curr = currTarget
+			
+			# Instantiate and configure the arrow
+			var tempArrow = Arrow.instantiate()
+			print("Arrow instantiated")
+			tempArrow.pathName = pathName
+			tempArrow.ArrowDamage = ArrowDamage
+			get_node("ArrowContainer").add_child(tempArrow)
+			tempArrow.global_position = $Aim.global_position
+		else:
+			print("No valid target found")
 		
-		var tempArrow = Arrow.instantiate()
-		tempArrow.pathName = pathName
-		tempArrow.ArrowDamage = ArrowDamage
-		get_node("ArrowContainer").add_child(tempArrow)
-		tempArrow.global_position = $Aim.global_position
-		
-		
+
 func _on_tower_body_exited(body):
-	pass # Replace with function body.
+	pass
