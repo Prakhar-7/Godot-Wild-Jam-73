f012ac281f61b96fc9f64c3810267694ef612611
Aditya Kumar Mishra <adityakr159@gmail.com>
2024-09-22 21:00:15 +0530

    tower check

--- castle_panel.gd
+++ castle_panel.gd
@@ -3,8 +3,18 @@ extends Panel
 @onready var tower = preload("res://scenes/castle.tscn")
 var currTile
 
+var valid_tiles = [Vector2i(10, 0), Vector2i(10, 1), Vector2i(11, 0), Vector2i(11, 1)]
+var tower_size = Vector2i(2.5, 2.5)
+
+
 func _on_gui_input(event):
+	var mapPath = get_tree().get_root().get_node("Level 1/TileMap")
+	var tile = mapPath.local_to_map(get_global_mouse_position())
+		
+	currTile = mapPath.get_cell_atlas_coords(0, tile, false)
 	var tempTower = tower.instantiate()
+	
+	var tower_position = mapPath.local_to_map(get_global_mouse_position())
 	if event is InputEventMouseButton and event.button_mask == 1:
 		#Left Click Down
 		add_child(tempTower)
@@ -15,17 +25,42 @@ func _on_gui_input(event):
 		#Left Click Down Drag
 		if get_child_count() > 1:
 			get_child(1).global_position = event.global_position
+		
+		if can_place_tower(currTile, mapPath):
+			get_child(1).get_node("Area").modulate = Color(0,255,0 , 0.1)
+		else:
+			get_child(1).get_node("Area").modulate = Color(255, 0, 0 , 0.4)
 			
+			
+		
 	elif event is InputEventMouseButton and event.button_mask == 0:
 		#Left Click up
 		#print("Left button up")
 		if get_child_count() > 1:
 			get_child(1).queue_free()
-		var path = get_tree().get_root().get_node("Level 1/Castles")
 		
-		path.add_child(tempTower)
-		tempTower.global_position = event.global_position
-		tempTower.get_node("Tower").hide()
+		
+		
+		if can_place_tower(currTile, mapPath):
+			var path = get_tree().get_root().get_node("Level 1/Castles")
+			
+			path.add_child(tempTower)
+			tempTower.global_position = event.global_position
+			tempTower.get_node("Area").hide()
 	else:
 		if get_child_count() > 1:
 			get_child(1).queue_free()
+
+
+func can_place_tower(top_left_tile: Vector2i, tilemap: TileMap) -> bool:
+	for x in range(tower_size.x):
+		for y in range(tower_size.y):
+			var tile_position = top_left_tile + Vector2i(x, y)
+
+			# Check if tile is valid
+			if tile_position not in valid_tiles:
+				return false  # If any tile is invalid, return false
+
+	return true  # All tiles are valid
+
+
