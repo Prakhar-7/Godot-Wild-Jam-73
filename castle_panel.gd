03c4be7dcdb2df90792c7cc15f77ab1506db96f1
Aditya Kumar Mishra <adityakr159@gmail.com>
2024-09-22 18:34:05 +0530

    .

--- castle_panel.gd
+++ castle_panel.gd
@@ -1,7 +1,31 @@
 extends Panel
 
+@onready var tower = preload("res://scenes/castle.tscn")
+var currTile
 
 func _on_gui_input(event):
-	#print(event)
-	pass
-
+	var tempTower = tower.instantiate()
+	if event is InputEventMouseButton and event.button_mask == 1:
+		#Left Click Down
+		add_child(tempTower)
+		tempTower.process_mode = Node.PROCESS_MODE_DISABLED
+		
+		
+	elif event is InputEventMouseMotion and event.button_mask == 1:
+		#Left Click Down Drag
+		if get_child_count() > 1:
+			get_child(1).global_position = event.global_position
+			
+	elif event is InputEventMouseButton and event.button_mask == 0:
+		#Left Click up
+		#print("Left button up")
+		if get_child_count() > 1:
+			get_child(1).queue_free()
+		var path = get_tree().get_root().get_node("Level 1/Castles")
+		
+		path.add_child(tempTower)
+		tempTower.global_position = event.global_position
+		tempTower.get_node("Tower").hide()
+	else:
+		if get_child_count() > 1:
+			get_child(1).queue_free()
--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 
-
+var player_health = 10
 
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://scenes/mainmenu.tscn")
+
