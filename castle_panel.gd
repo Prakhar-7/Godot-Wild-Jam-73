12c8c9e366ced60298d25ab3940f5d815fa37449
Prakhar Srivastava <sriprakhar777@gmail.com>
2024-09-22 17:15:24 +0530

    Add Drag and Drop Feature

--- castle_panel.gd
+++ castle_panel.gd
@@ -1,6 +1,25 @@
 extends Panel
 
+@onready var tower = preload("res://scenes/castle.tscn")
+var currTile
 
 func _on_gui_input(event):
-	print(event)
-
+	var tempTower = tower.instantiate()
+	if event is InputEventMouseButton and event.button_mask == 1:
+		#Left Click Down
+		add_child(tempTower)
+		tempTower.process_mode = Node.PROCESS_MODE_DISABLED
+		tempTower.scale = Vector2(0.10,0.10)
+		
+	elif event is InputEventMouseMotion and event.button_mask == 1:
+		#Left Click Down Drag
+		get_child(1).global_position = event.global_position
+	elif event is InputEventMouseButton and event.button_mask == 0:
+		#Left Click up
+		print("Left button up")
+		get_child(1).queue_free()
+		var path = get_tree().get_root().get_node("Level 1/Castle")
+		
+		path.add_child(tempTower)
+		tempTower.global_position = event.global_position
+		tempTower.get_node("Tower").hide()
--- scripts/shop_item.gd
+++ scripts/shop_item.gd
@@ -5,5 +5,9 @@ var currTile
 
 
 func _on_gui_input(event):
+	#print(event.button_index)
 	var tempTower = tower.instantiate()
-	print(event)
+	if event is InputEventMouseButton and event.button_index == 1:
+		#print("Left button down")
+	elif event is InputEventMouseMotion and event.button_mask == 0:
+		#print("Left button up")
