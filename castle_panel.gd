ec31355b06e624ad23db81324a5a19e0e2b40f68
Prakhar Srivastava <sriprakhar777@gmail.com>
2024-09-22 17:59:55 +0530

    Unnecessary

--- castle_panel.gd
+++ castle_panel.gd
@@ -13,13 +13,19 @@ func _on_gui_input(event):
 		
 	elif event is InputEventMouseMotion and event.button_mask == 1:
 		#Left Click Down Drag
-		get_child(1).global_position = event.global_position
+		if get_child_count() > 1:
+			get_child(1).global_position = event.global_position
+			
 	elif event is InputEventMouseButton and event.button_mask == 0:
 		#Left Click up
-		print("Left button up")
-		get_child(1).queue_free()
+		#print("Left button up")
+		if get_child_count() > 1:
+			get_child(1).queue_free()
 		var path = get_tree().get_root().get_node("Level 1/Castle")
 		
 		path.add_child(tempTower)
 		tempTower.global_position = event.global_position
 		tempTower.get_node("Tower").hide()
+	else:
+		if get_child_count() > 1:
+			get_child(1).queue_free()
